0c4e80c7d0f35cbd6eecd2cab5e0ecba263226b4
Vasecot <51337977+Vasecot@users.noreply.github.com>
2019-08-21 15:42:49 +0400

    Завершена шапка и навигация

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 gulp.task("sass", function() {
-  gulp.src("sass/styles.scss")
+  gulp.src("sass/style.scss")
     .pipe(sass())
     .pipe(gulp.dest("css"));
 });
